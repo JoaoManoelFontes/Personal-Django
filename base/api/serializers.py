279e3c61ccdfccc082e0edcc602a2664fdea9aca
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-09-01 18:00:45 -0300

    add authentication on api

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,8 +1,30 @@
+from dataclasses import field
+from email import message
 from rest_framework.serializers import ModelSerializer
-from base.models import Room
+from rest_framework.fields import SerializerMethodField
+from django.utils.timezone import now
+from base.models import Room, Topic, Message
+from django.contrib.auth.models import User
+
+
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ('name',)
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class RoomSerializer(ModelSerializer):
+    #? serializando os models do relacionamento - nested serializers
+    topic = TopicSerializer()
+    host = UserSerializer()
+    days_since_created = SerializerMethodField()
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = ('name', 'description', 'days_since_created', 'topic', 'host')
        
+    def get_days_since_created(self, obj):
+        return ('%s days' % (now() - obj.created).days )
--- base/api/urls.py
+++ base/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import views
 
 urlpatterns = [
     path('', views.getRooms),
     path('room/<str:roomId>/', views.getRoom),
-    path('topics/<str:topicId>/', views.getRoomsForTopic)
+    path('topics/<str:topicId>/', views.getRoomsForTopic),
+    path('dou/', views.get_data_from_api)
 ]
